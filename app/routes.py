8c20bb680ed782a1e117c8727273107c4eca62ba
nicholasWijaya100 <88177604+nicholasWijaya100@users.noreply.github.com>
2024-08-21 14:40:06 +0700

    added login register two roles

--- app/routes.py
+++ app/routes.py
@@ -9,9 +9,10 @@ from datetime import datetime, timedelta
 from collections import Counter
 from flask import Blueprint, render_template, flash, redirect, request, url_for, Response, stream_with_context, abort, jsonify
 from app.extensions import db
-from app.models import Camera, Contact, DetectedObject, Detector
-from app.forms import AddCCTVForm, EditCCTVForm, SelectCCTVForm, ContactForm, DetectorForm    
+from app.models import Camera, Contact, DetectedObject, Detector, User
+from app.forms import AddCCTVForm, EditCCTVForm, SelectCCTVForm, ContactForm, DetectorForm, LoginForm, RegistrationForm 
 from app import gesture_detector, ppe_detector, unfocused_detector
+from flask_login import current_user, login_user, logout_user, login_required
 
 main = Blueprint('main', __name__)
 current_user = {'name': 'John'}
@@ -105,6 +106,7 @@ GRAPHICS_DIR = 'app/static/graphics'
 #                            graph4=graph4)
 
 @main.route('/')
+@login_required
 def index():
     # Fetching data
     num_cctv = Camera.query.count()
