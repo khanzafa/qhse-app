0428657501e8f78ea9e908e4282dcd5ed54f0808
Ubuntu <ubuntu@cctv-hcca.asia-southeast2-a.c.spilcloud.internal>
2024-10-28 07:12:22 +0000

    requirements

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ COPY . /app
 
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 libgl1 libpq-dev musl-dev -y 
 
-RUN pip install --no-cache-dir -r linux-requirements.txt
+RUN pip install --no-cache-dir -r docker-requirements.txt
 
 EXPOSE 5000
 
 ENV FLASK_APP=app/__init__.py
 
-CMD ["flask", "run", "--host=0.0.0.0"]
+CMD ["flask", "run", "--host=10.184.0.32"]
--- app/__init__.py
+++ app/__init__.py
@@ -131,8 +131,8 @@ def create_app():
 
 
     # Jalankan thread detektor sebelum memulai Flask
-    detector_thread = threading.Thread(target=run_detectors, args=(app,), name="DetectorThread")
-    detector_thread.start()
+    #detector_thread = threading.Thread(target=run_detectors, args=(app,), name="DetectorThread")
+    #detector_thread.start()
 
     # Tangkap sinyal SIGINT (Ctrl+C) dan SIGTERM untuk menghentikan detektor saat server dihentikan
     signal.signal(signal.SIGINT, handle_shutdown_signal)
